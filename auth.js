85b7d7bbf151079bab7082ade4e28a6072847472
soun Soratha <121934976+ZensYing@users.noreply.github.com>
2024-03-20 17:02:33 +0700

    updatae alert user

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 // auth.js
 document.addEventListener('DOMContentLoaded', (event) => {
     if(localStorage.getItem('isLoggedIn') !== 'true') {
+        alert("Login first to access the Homepage")
         window.location.href = 'index.html'; // Redirect to login page
     }
 });
